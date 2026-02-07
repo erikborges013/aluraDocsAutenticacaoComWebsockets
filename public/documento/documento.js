58ccb15cd7b21f78eabbf4b487cc3be7cea2a66e
erikborges013 <erikborgesofficial@gmail.com>
2026-02-07 01:26:00 -0300

    Upload do projeto prático final websockets

---
+++ public/documento/documento.js
@@ -0,0 +1,52 @@
+import {
+  emitirExcluirDocumento,
+  emitirTextoEditor,
+  selecionarDocumento,
+} from "./socket-front-documento.js";
+
+const parametros = new URLSearchParams(window.location.search);
+const nomeDocumento = parametros.get("nome");
+
+const textoEditor = document.getElementById("editor-texto");
+const tituloDocumento = document.getElementById("titulo-documento");
+const botaoExcluir = document.getElementById("excluir-documento");
+const listaUsuariosConectados = document.getElementById("usuarios-conectados");
+
+tituloDocumento.textContent = nomeDocumento || "Documento sem título";
+
+function tratarAutorizacaoSucesso(payloadToken) {
+  selecionarDocumento({nomeDocumento, nomeUsuario: payloadToken.nomeUsuario}); 
+}
+
+function atualizarInterfaceUsuarios(usuariosNoDocumento) {
+  let usuarios = "";
+  listaUsuariosConectados.innerHTML = "";
+  usuariosNoDocumento.forEach((usuario) => {
+    usuarios += `<li class="list-group-item">${usuario}</li>`;
+  });
+  listaUsuariosConectados.innerHTML = usuarios;
+}
+
+textoEditor.addEventListener("keyup", () => {
+  emitirTextoEditor({
+    texto: textoEditor.value,
+    nomeDocumento,
+  });
+});
+
+function atualizaTextoEditor(texto) {
+  textoEditor.value = texto;
+}
+
+botaoExcluir.addEventListener("click", () => {
+  emitirExcluirDocumento(nomeDocumento);
+});
+
+function alertarERedirecionar(nome) {
+  if (nome === nomeDocumento) {
+    alert(`Documento ${nome} excluído!`);
+    window.location.href = "/";
+  }
+}
+
+export { atualizaTextoEditor, alertarERedirecionar, tratarAutorizacaoSucesso, atualizarInterfaceUsuarios };
